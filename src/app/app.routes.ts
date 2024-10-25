e51507c78f564f791ace4f0f10db0096e33fbbfa
Carlos Castro <robertoccg@proton.me>
2024-10-24 21:19:32 -0700

    Adding page folders with home, products, cart, page-not-found

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { ProductsComponent } from './pages/products/products.component';
+import { HomeComponent } from './pages/home/home.component';
+import path from 'path';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { CartComponent } from './pages/cart/cart.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', component: HomeComponent, 
+    },
+
+    {
+        path: 'home', component: HomeComponent,
+    }, 
+    {
+        path: 'products', component: ProductsComponent,
+    },
+    {
+        path: 'cart', component: CartComponent,
+    }, 
+    {
+        path: '**', component: PageNotFoundComponent, 
+    }
+
+];
